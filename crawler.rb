a8e83afb1510a55327096df755b9d5ae56abd4ab
Michel Kim <kim98.michel@gmail.com>
2024-01-15 21:12:57 -0300

    fetch all pokemons from poke api and export json file

--- crawler.rb
+++ crawler.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Pokemon
   attr_accessor :name, :height, :weight, :types
@@ -9,43 +10,73 @@ class Pokemon
     @weight = weight
     @types = types.map { |type| type['type']['name']}
   end
+
+  def to_json(*_args)
+    {
+      name: @name,
+      height: @height,
+      weight: @weight,
+      types: @types,
+      moves: @moves
+    }.to_json
+  end
 end
 
 url = "https://pokeapi.co/api/v2/pokemon"
 
-response = HTTParty.get(url)
+pokemons = []
 
-if response.success?
-  data = response.parsed_response
-
-  pokemons = []
-
-  data['results'].each do |pokemon|
-    url_response = HTTParty.get(pokemon['url'])
-
-    if url_response.success?
-
-      poke_data = url_response.parsed_response
-
-      poke_objc = Pokemon.new(poke_data['name'], poke_data['height'], poke_data['weight'], poke_data['types'])
-
-      pokemons << poke_objc
-    else
-      puts "Erro ao obter dados da URL #{pokemon['url']}"
+loop do
+  
+  puts "Executando chamada na url: #{url}"
+  response = HTTParty.get(url)
+  
+  if response.success?
+    data = response.parsed_response
+  
+    data['results'].each do |pokemon|
+      url_response = HTTParty.get(pokemon['url'])
+  
+      if url_response.success?
+  
+        poke_data = url_response.parsed_response
+  
+        poke_objc = Pokemon.new(poke_data['name'], poke_data['height'], poke_data['weight'], poke_data['types'])
+  
+        pokemons << poke_objc
+      else
+        puts "Erro ao obter dados da URL #{pokemon['url']}"
+      end
     end
-  end
 
-  pokemons.each do |pokemon|
-    pokemon_hash = {
-      name: pokemon.name,
-      height: pokemon.height,
-      weight: pokemon.weight,
-      types: pokemon.types
-    }
+    next_url = data['next']
+    break if next_url.nil?
 
-    puts JSON.pretty_generate(pokemon_hash)
+    url = next_url
+
+  else
+    puts "Erro na chamada da API. Código de status: #{response.code}"
+    puts "Resposta da API: #{response.body}"
   end
-else
-  puts "Erro na chamada da API. Código de status: #{response.code}"
-  puts "Resposta da API: #{response.body}"
+  
 end
+
+pokemons.each do |pokemon|
+  pokemon_hash = {
+    name: pokemon.name,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    types: pokemon.types
+  }
+  puts JSON.pretty_generate(pokemon_hash)
+end
+
+puts "#{pokemons.length} Pokemons crawleados."
+
+# Exportar como json file
+
+json_content = JSON.pretty_generate(pokemons)
+
+File.open('pokemons.json', 'w') {|file| file.write(json_content)}
+
+puts 'Arquivo JSON salvo com sucesso.'
